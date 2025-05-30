cdae6438e9f6263511cb6f59e07de199699aecb2
Samy <152478286+SamyGoumiri@users.noreply.github.com>
2025-05-30 10:51:46 +0100

    Update bot.py

--- bot.py
+++ bot.py
@@ -2,19 +2,26 @@ import os
 from dotenv import load_dotenv
 import discord
 from discord.ext import commands
+from discord import app_commands
 
 load_dotenv()  # charge le .env
 token = os.getenv('DISCORD_TOKEN')
 
 intents = discord.Intents.default()
 bot = commands.Bot(command_prefix='!', intents=intents)
+tree = app_commands.CommandTree(bot)
 
 @bot.event
 async def on_ready():
     print(f'Connecté en tant que {bot.user}!')
+    await tree.sync()
 
 @bot.command()
 async def ping(ctx):
     await ctx.send('Pong!')
+
+@tree.command(name="ping", description="Répond Pong!")
+async def ping_slash(interaction: discord.Interaction):
+    await interaction.response.send_message("Pong!")
 
 bot.run(token)
