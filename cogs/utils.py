f30a485f4a7c77eb9f0ad5382486230daae75b41
Samy <152478286+SamyGoumiri@users.noreply.github.com>
2025-05-30 15:58:38 +0200

    e

--- cogs/utils.py
+++ cogs/utils.py
@@ -6,10 +6,6 @@ class Utils(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
-    @commands.command()
-    async def ping(self, ctx):
-        await ctx.send('Pong!')
-
     @app_commands.command(name="ping", description="Répond Pong!")
     async def ping_slash(self, interaction: discord.Interaction):
         await interaction.response.send_message("Pong!")
